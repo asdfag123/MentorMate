af6856b2427ff325a1ab7c5fdd2fb255114bfb14
Dimitar Anastasov <33731426+asdfag123@users.noreply.github.com>
2019-05-29 20:18:41 +0300

    Add files via upload

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+public class Employee {
+
+    private String name;
+    private int totalSales;
+    private int salesPeriod;
+    private double experienceMultiplier;
+    private double score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(int totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public int getSalesPeriod() {
+        return salesPeriod;
+    }
+
+    public void setSalesPeriod(int salesPeriod) {
+        this.salesPeriod = salesPeriod;
+    }
+
+    public double getExperienceMultiplier() {
+        return experienceMultiplier;
+    }
+
+    public void setExperienceMultiplier(double experienceMultiplier) {
+        this.experienceMultiplier = experienceMultiplier;
+    }
+
+    public double getScore(){
+        return score;
+    }
+
+    public void setScore(double score){
+        this.score = score;
+    }
+}
